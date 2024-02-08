18f5877630c4ea86d835dc77acc800ff0b62f500
Michele Codes <hellomichelecodes@gmail.com>
2024-02-07 23:06:10 -0300

    first

---
+++ CadastroCliente/src/dao/IClienteDAO.java
@@ -0,0 +1,21 @@
+package br.com.michele.dao;
+
+import br.com.michele.domain.Cliente;
+
+import java.util.Collection;
+
+/**
+ * @author michele.codes
+ */
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
